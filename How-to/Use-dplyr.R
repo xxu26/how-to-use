155dce3de0a8698d19ecf260575f3ecfa386fb3a
Xiangyi Xu (Lydia) <xxu26@dons.usfca.edu>
2016-02-22 14:01:26 -0800

    Update Use-dplyr.R

--- How-to/Use-dplyr.R
+++ How-to/Use-dplyr.R
@@ -6,9 +6,7 @@ str(crime.by.state)
 head(crime.by.state)
 # base R
 crime.by.state <- read.csv("CrimeStatebyState.csv")
-crime.ny.2005 <- crime.by.state[crime.by.state$Year==2005 &
-                                        crime.by.state$State=="New York", 
-                                c("Type.of.Crime", "Count")]
+crime.ny.2005 <- crime.by.state[crime.by.state$Year==2005 & crime.by.state$State=="New York", c("Type.of.Crime", "Count")]
 crime.ny.2005 <- crime.ny.2005[order(crime.ny.2005$Count, decreasing=TRUE), ]
 crime.ny.2005$Proportion <- crime.ny.2005$Count/sum(crime.ny.2005$Count)
 summary1 <- aggregate(Count ~ Type.of.Crime, data=crime.ny.2005, FUN=sum)
