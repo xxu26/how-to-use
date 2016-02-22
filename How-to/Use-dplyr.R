080b1c1204d9f4dd899a27eafccc70bb260947cc
Xiangyi Xu (Lydia) <xxu26@dons.usfca.edu>
2016-02-22 14:17:18 -0800

    added step by step comparison

--- How-to/Use-dplyr.R
+++ How-to/Use-dplyr.R
@@ -27,3 +27,40 @@ final
 str(crime.by.state)
 select(crime.by.state, Type.of.Crime, Count)
 filter(crime.by.state, State=="New York", Year==2005)
+
+
+#Filtering rows
+# base R
+crime.ny.2005 <- crime.by.state[crime.by.state$Year==2005 &
+                                        crime.by.state$State=="New York", ]
+# dplyr
+crime.ny.2005 <- filter(crime.by.state, State=="New York", Year==2005)
+
+#Arranging and ordering
+# base R
+crime.ny.2005 <- crime.ny.2005[order(crime.ny.2005$Count, decreasing=TRUE), ]
+# dplyr
+crime.ny.2005 <- arrange(crime.ny.2005, desc(Count))
+
+
+#Selecting columns
+# base R
+crime.ny.2005 <- crime.ny.2005[, c("Type.of.Crime", "Count")]
+# dplyr
+crime.ny.2005 <- select(crime.ny.2005, Type.of.Crime, Count)
+
+
+#Creating new columns
+# base R
+crime.ny.2005$Proportion <- crime.ny.2005$Count/sum(crime.ny.2005$Count)
+# dplyr
+crime.ny.2005 <- mutate(crime.ny.2005, Proportion=Count/sum(Count))
+
+#Aggregation and summarization
+# base R
+summary1 <- aggregate(Count ~ Type.of.Crime,data=crime.ny.2005,FUN=sum)
+summary2 <- aggregate(Count ~ Type.of.Crime, data=crime.ny.2005,FUN=length)
+summary.crime.ny.2005 <- merge(summary1, summary2,by="Type.of.Crime")
+# dplyr
+by.type <- group_by(crime.ny.2005, Type.of.Crime)
+summary.crime.ny.2005 <- summarise(by.type,num.types = n(),counts = sum(Count))
